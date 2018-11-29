b1cafb8535506cf924e867466dd4bdb97238651c
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2018-11-29 16:45:52 +0100

    feat(article): Add Title value object

---
+++ src/Article/ValueObject/Exception/TitleTooShort.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Francesco.Zanoni
+ * Date: 29/11/2018
+ * Time: 16:12
+ */
+
+namespace LaravelDay\Article\ValueObject\Exception;
+
+
+class TitleTooShort
+{
+
+}
